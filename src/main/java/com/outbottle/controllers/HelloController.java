403c44c65bcf14f6e8eb48103582c2aff92b1cf9
Frank <zhoufan879@126.com>
2015-01-06 15:17:08 +0800

    首次提交

---
+++ src/main/java/com/outbottle/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.outbottle.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.ApiOperation;
+
+@RestController
+public class HelloController {
+    
+	@RequestMapping(method=RequestMethod.GET,value="saurzcode/hello")
+	@ApiOperation(httpMethod = "GET", value = "Say Hello To World using Swagger")
+	public String sayHello() {
+		return "Greetings from SaurzCode! ";
+	}
+    
+}
